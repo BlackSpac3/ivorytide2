dc024a19fb7b29457f637560ae4b13c98fe6a1e4
BlackSpac3 <toyinacobs12@gmail.com>
2025-10-03 14:19:32 +0100

    trimming input fields

--- schemas/rsvp.schema.ts
+++ schemas/rsvp.schema.ts
@@ -3,8 +3,8 @@ import z from "zod";
 
 const RsvpFormSchema = z.object({
   title: z.enum(titleOptions, { message: "Title is required" }),
-  first_name: z.string().min(1, { message: "First name is required" }),
-  last_name: z.string().min(1, { message: "Last name is required" }),
+  first_name: z.string().trim().min(1, { message: "First name is required" }),
+  last_name: z.string().trim().min(1, { message: "Last name is required" }),
   is_attending: z.enum(["yes", "no"], { message: "Please select an option" }),
   reason: z.string().optional(),
 });
